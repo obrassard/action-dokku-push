0e7b0e241ea2f807abf8c9b48dc8c0f7ae26728b
Olivier Brassard <brassard.oli@gmail.com>
2020-03-13 22:09:55 -0400

    SSH add

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh_host=$(echo $DOKKU_REPO | sed 's/.*@//' | sed 's/[:/].*//')
 ssh-keyscan -H "$ssh_host" >> ~/.ssh/known_hosts
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
 
 echo "Pushing to Dokku Host";
 
