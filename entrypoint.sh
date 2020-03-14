bdf2d201d3326966e8e1060c2c018b859ff16e15
Olivier Brassard <brassard.oli@gmail.com>
2020-03-13 22:04:50 -0400

    Fix known_host issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh_host=$(echo $DOKKU_REPO | sed 's/.*@//' | sed 's/[:/].*//')
-ssh-keyscan -H 22 "$ssh_host" >> ~/.ssh/known_hosts
+ssh-keyscan -H "$ssh_host" >> ~/.ssh/known_hosts
 
 echo "Pushing to Dokku Host";
 
